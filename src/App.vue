04191ff325af05bf1b968153f5c6f30fc044cb84
Thonthus <thonthus2546@gmail.com>
2023-10-15 19:21:30 +0700

    update footer

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,19 @@ import HeaderPart from './components/HeaderPart.vue'
       <a href="/" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
         <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
       </a>
-      <span class="mb-3 mb-md-0">© บริษัท ตั้งหวังเซ้ง จำกัด</span>
+      <span class="mb-3 mb-md-0 " style="font-weight: 700;">© บริษัท ตั้งหวังเซ้ง จำกัด</span>
       <span class="mb-3 mb-md-0">โทร 064 843 2286</span>
+      <br> 
+      <ul style="list-style: none; text-align: end;">
+            <li style="font-weight: 600;">คณะผู้บริหาร</li>
+            <li style="font-weight: 500;">ศ.ดร.มนภัทร มั่นคง <span style="font-weight: 400;">กรรมการและผู้จัดการร้านครัวคุณบิ๋ม</span></li>
+            <li style="font-weight: 500;">ศ.ดร.ปิยะวัฒน์ บัวทอง <span style="font-weight: 400;">รองผู้จัดการ</span></li>
+            <li style="font-weight: 500;">พล.อ.พิริยะ อินขาน <span style="font-weight: 400;">รองผู้จัดการ</span></li>
+            <li style="font-weight: 500;">พล.อ.อ.วีรภัทร ลาภคุณ <span style="font-weight: 400;">รองผู้จัดการ</span></li>
+            <li style="font-weight: 500;">พล.ร.อ.ศ.ดร.ธรณ์ธรรศ ปรางเพ็ชร <span style="font-weight: 400;">คนขับรถ</span></li>
+        </ul>
     </div>
+    
   </footer>
 
 
